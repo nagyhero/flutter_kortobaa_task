8e12542faa6c5fd74b8b2098a4bd8f070f810a81
ahmednagy <ahmednagyhero25@gmail.com>
2020-08-23 21:20:34 +0200

    add screen_util lib , add tabs containers

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,8 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kortobaa_task/src/tabs/main_tab.dart';
+import 'package:flutter_kortobaa_task/src/tabs/profile_tab.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    ScreenUtil.init(context);
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        endDrawer: _drawer,
+        body: TabBarView(
+          children: <Widget>[
+            MainTab(),
+            ProfileTab(),
+          ],
+        ),
+
+      ),
+    );
   }
+
+  Drawer _drawer = Drawer(
+
+  );
 }
